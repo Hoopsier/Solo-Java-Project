a773525fa194027efefe7d754f5da28d1eeffb7e
Hoopsy <renanhoruz@gmail.com>
2026-04-21 22:19:49 +0300

    chore: queries for db

---
+++ Queries/Project.sql
@@ -0,0 +1,42 @@
+DROP database if exists simulation;
+create database simulation;
+DROP USER IF EXISTS 'appuser'@'localhost';
+CREATE USER 'appuser'@'localhost' IDENTIFIED BY 'password';
+GRANT SELECT, INSERT, UPDATE, DELETE ON simulation.* TO 'appuser'@'localhost';
+
+USE simulation;
+
+create table service_point(
+  id int not null AUTO_INCREMENT,
+  arrived int not null,
+  serviced int not null,
+  active_time bigint not null,
+  total_time bigint not null,
+  PRIMARY KEY(id)
+);
+
+create table customer(
+  id int not null AUTO_INCREMENT,
+  response_time int not null,
+  service_id int,
+  PRIMARY KEY(id),
+  FOREIGN KEY(service_id) REFERENCES service_point(id)
+);
+
+-- NOTE: the following is an example of how to use the tables
+-- Also note the order of operations, especially with the foreign key
+
+INSERT INTO service_point (
+  arrived, serviced, active_time,total_time
+) VALUES ( 123, 123, 123123, 123321 );
+
+select * from service_point;
+
+INSERT INTO customer (
+  response_time,service_id
+) VALUES ( 123123, 1 );
+
+select * from customer;
+DELETE FROM customer;
+DELETE FROM service_point;
+
