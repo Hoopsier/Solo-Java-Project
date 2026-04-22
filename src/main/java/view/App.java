819b9a8d06e0f0f31842b380f035012c78799664
Hoopsy <renanhoruz@gmail.com>
2026-04-22 16:02:49 +0300

    feat: UI.fxml start up done   chore: dependencies are finished

--- src/main/java/view/App.java
+++ src/main/java/view/App.java
@@ -1,10 +1,23 @@
 package view;
 
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * Hello world!
  */
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+public class App extends Application {
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+  public void start(Stage stage) {
+    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/UI.fxml"));
+
+    System.out.println("Hello World!");
+  }
 }
