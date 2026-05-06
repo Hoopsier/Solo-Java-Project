9e1007b647420c53c7ff4ee5d3a65e04606538ce
Renan Horuz <76113327+Hoopsier@users.noreply.github.com>
2026-05-06 14:55:41 +0300

    Prevent simulation time from moving backward

--- src/main/java/model/phases/APhase.java
+++ src/main/java/model/phases/APhase.java
@@ -4,7 +4,8 @@ import java.util.List;
 
 public class APhase {
 
-  public static int activate(List<Integer> aSet) {
+  public static int activate(List<Integer> aSet, int currentTime) {
+    aSet.removeIf(time -> time < currentTime);
     int time = aSet.get(0);
     aSet.remove(0);
     return time;
