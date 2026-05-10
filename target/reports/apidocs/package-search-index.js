96fe8135f709e916eac183b64d9d48dea4b01050
Hoopsy <renanhoruz@gmail.com>
2026-05-10 14:11:35 +0300

    APIDocs at target/reports/apidocs/index.html

---
+++ target/reports/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"controller"},{"l":"model"},{"l":"model.database"},{"l":"model.phases"},{"l":"model.serviceObjects"},{"l":"view"}];updateSearchResults();
